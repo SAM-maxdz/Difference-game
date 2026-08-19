256a528b418a29b7b96fe14adc2c7a201091aac4
SAM-maxdz <dmtditn@gmail.com>
2026-08-19 02:02:16 +0100

    Create Layout.js

---
+++ App/Layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Difference Game",
+  description: "A multiplayer spot-the-difference game",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ar">
+      <body>{children}</body>
+    </html>
+  );
+}
